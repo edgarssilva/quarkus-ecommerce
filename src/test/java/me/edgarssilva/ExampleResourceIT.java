90ed2b6acd15114edcdbd91a1618d602892cda90
edgarssilva <edgarasdaa10@gmail.com>
2024-10-03 22:00:30 +0100

    Kubernetes and Minikube

--- src/test/java/me/edgarssilva/ExampleResourceIT.java
+++
@@ -1,8 +0,0 @@
-package me.edgarssilva;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class ExampleResourceIT extends ExampleResourceTest {
-    // Execute the same tests but in packaged mode.
-}
