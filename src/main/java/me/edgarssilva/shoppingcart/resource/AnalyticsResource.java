c7dfae920c29de33536dd46d29a439841564be24
edgarssilva <edgarasdaa10@gmail.com>
2024-10-04 13:40:33 +0100

    README.md and kubernetes/openapi example

--- src/main/java/me/edgarssilva/shoppingcart/resource/AnalyticsResource.java
+++ src/main/java/me/edgarssilva/shoppingcart/resource/AnalyticsResource.java
@@ -6,6 +6,7 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
 import me.edgarssilva.shoppingcart.model.KpiMetrics;
 import me.edgarssilva.shoppingcart.service.AnalyticsService;
+import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
@@ -17,6 +18,7 @@ public class AnalyticsResource {
     AnalyticsService service;
 
     @GET
+    @Operation(summary = "Get key performance indicators (KPIs)")
     @APIResponse(responseCode = "200", description = "Retrieves key performance indicators (KPIs) for shopping carts.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = KpiMetrics.class)))
     public Response getKPIs() {
         return Response.ok(service.getKPIs()).build();
